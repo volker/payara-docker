ef91963cb409be9b5db490c1716c1105d6566fb7
Mike Croft <backtotheoldhouse@gmail.com>
2015-08-14 12:26:59 +0100

    fixed success check to look for exit code, not existance of image

--- openjdk-8/buildimage.sh
+++ openjdk-8/buildimage.sh
@@ -3,11 +3,12 @@ IMAGE_NAME=payara:4.1.152.1.openjdk8
 
 docker build -t $IMAGE_NAME .
 
-if [ ! -f $IMAGE_NAME ]
+if [ $? -ne 0 ]
 then
     echo "Build failed!"
 else
-    echo "Payara 4.1.152.1 docker image has been built.\nTo start a new container, execute: docker run -i -t payara:4.1.152.1.openjdk8  /bin/bash "
+    echo "Payara 4.1.152.1 docker image has been built."
+    echo "To start a new container, execute: docker run -i -t payara:4.1.152.1.openjdk8  /bin/bash "
 fi
 
 
