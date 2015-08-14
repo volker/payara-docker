114dce010155110893e4ff6e37c029efb5f9ff52
Mike Croft <backtotheoldhouse@gmail.com>
2015-08-14 11:30:50 +0100

    modified build image to check for success

--- openjdk-8/buildimage.sh
+++ openjdk-8/buildimage.sh
@@ -3,6 +3,11 @@ IMAGE_NAME=payara:4.1.152.1.openjdk8
 
 docker build -t $IMAGE_NAME .
 
-echo "Payara 4.1.152.1 docker image has been built. To start a new container, execute: docker run -i -t payara:4.1.152.1.openjdk8  /bin/bash "
+if [ ! -f $IMAGE_NAME ]
+then
+    echo "Build failed!"
+else
+    echo "Payara 4.1.152.1 docker image has been built.\nTo start a new container, execute: docker run -i -t payara:4.1.152.1.openjdk8  /bin/bash "
+fi
 
 
