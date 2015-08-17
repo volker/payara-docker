5ec7d001268615cad60e9427cb9b16b37bde3aa4
Mike Croft <backtotheoldhouse@gmail.com>
2015-08-17 18:53:53 +0100

    new dockerfile built on Azul's Zulu build of OpenJDK

---
+++ zulu-8/buildimage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+IMAGE_NAME=payara:4.1.152.1.zulu8
+
+docker build -t $IMAGE_NAME .
+
+if [ $? -ne 0 ]
+then
+    echo "Build failed!"
+else
+    echo "Payara 4.1.152.1 docker image has been built."
+    echo "To start a new container, execute: docker run -i -P -t payara:4.1.152.1.zulu8  /bin/bash "
+fi
+
+
