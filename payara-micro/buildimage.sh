7c1a0cf5dd36b4d7d3cb3d8f63a1482614df09c5
Steve Millidge <steve@payara.co>
2015-10-08 16:14:11 +0100

    Now Payara Micro deploys from /opt/payara-micro-wars

--- payara-micro/buildimage.sh
+++ payara-micro/buildimage.sh
@@ -3,6 +3,6 @@ IMAGE_NAME=payara:latest.micro.oraclejdk8
 
 docker build -t $IMAGE_NAME .
 
-echo "The payara-micro docker image has been built. To start a new container, execute: docker run -i -t payara:latest.micro.oraclejdk8  /bin/bash "
+echo "The payara-micro docker image has been built. To start a new container, execute: docker run -i -t payara:latest.micro.oraclejdk8"
 
 
