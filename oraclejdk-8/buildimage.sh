0a4abe89f3e0828f412a1553ed762e0b36d25e3d
Volker Fritzsch <volker.fritzsch@mercatis.com>
2016-05-01 21:58:55 +0200

    Payara 4.1.1.161.1

--- oraclejdk-8/buildimage.sh
+++ oraclejdk-8/buildimage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-IMAGE_NAME=payara:4.1.152.1.oraclejdk8
+IMAGE_NAME=payara:4.1.1.161.1.oraclejdk8
 
 docker build -t $IMAGE_NAME .
 
@@ -7,6 +7,6 @@ if [ $? -ne 0 ]
 then
     echo "Build failed!"
 else
-    echo "Payara 4.1.152.1 docker image has been built."
-    echo "To start a new container, execute: docker run -i -P -t payara:4.1.152.1.openjdk8  /bin/bash "
+    echo "Payara 4.1.1.161.1 docker image has been built."
+    echo "To start a new container, execute: docker run -i -P -t payara:4.1.1.161.1.oraclejdk8 /bin/bash "
 fi
